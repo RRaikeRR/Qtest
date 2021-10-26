7a9c9e554dd531d29c4848b2d98b655235f59b81
SavaSavovich <trusava123@gmail.com>
2021-10-26 09:56:33 +0300

    Add file

---
+++ mainwindow.h
@@ -0,0 +1,49 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+#include <QDebug>
+#include <QMainWindow>
+#include <QJsonObject>
+#include <QJsonParseError>
+#include <QFile>
+#include <QFileDialog>
+#include <QDebug>
+#include<QJsonDocument>
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+
+    QJsonDocument document;
+    QJsonDocument document1;
+    QString path;
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+
+    void on_pushButton_clicked();
+
+    void on_pushButton_11_clicked();
+
+    void on_pushButton_12_clicked();
+
+    void on_pushButton_13_clicked();
+
+    void on_pushButton_6_clicked();
+
+    void on_pushButton_7_clicked();
+
+    void on_pushButton_5_clicked();
+
+private:
+    Ui::MainWindow *ui;
+    int test = 1;
+    int table;
+};
+#endif // MAINWINDOW_H
